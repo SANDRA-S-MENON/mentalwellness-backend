b8b0012677ee230e9bfeba8a913e0343255f1d45
SANDRA-S-MENON <sandramenon2002@gmail.com>
2025-02-14 11:44:54 +0530

    project created

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bcrypt = require("bcrypt")
+const jsonwebtoken = require("jsonwebtoken")
+
+
+
+
+
+
+let app =express()
+
+app.get("/",(req,res)=>{
+        res.send("hello")
+})
+
+
+
+app.listen(3030,()=>{
+    console.log("server started")
+})
